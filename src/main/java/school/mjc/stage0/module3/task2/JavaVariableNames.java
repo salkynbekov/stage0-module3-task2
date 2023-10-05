5b624a1b818e348f38a0b54d8e0ff8355e99917e
Damir <salkynbekov.damir@gmail.com>
2023-10-05 21:27:22 +0600

    Done2

--- src/main/java/school/mjc/stage0/module3/task2/JavaVariableNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/JavaVariableNames.java
@@ -3,12 +3,12 @@ package school.mjc.stage0.module3.task2;
 public class JavaVariableNames {
     public static void main(String[] args) {
         int morrow = 0;
-        int $money = 10;
+        int money = 10;
         int hundred = 100;
         int back = 1000;
-        int _underscore = 10000;
+        int underscore = 10000;
         int sharp = 100000;
         int star = 1000000;
-        int BADEXAMPLEOFVARNAME = 1000000;
+        int badExampleOfVariable = 1000000;
     }
 }
